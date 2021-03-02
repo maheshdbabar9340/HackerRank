38cb4cbdf71d33aec65412ecda03b7a5ba5e6be8
Mahesh Dattatraya Babar <53929423+maheshdbabar9340@users.noreply.github.com>
2021-03-02 22:33:38 +0530

    15 th problem

---
+++ Java/Java String Reverse/Solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        int count = 0;
+        for(int i=0; i<A.length()/2;i++){
+            if( A.charAt(i) == A.charAt(A.length()-i-1)){
+                count++;
+            }
+        }
+        if(count == A.length()/2){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");            
+        }  
+    }
+}
